77ff1c3dd173e59970634ed45ef7e56b5c136a3b
hiseholuwa <hiseholuwa@outlook.com>
2017-08-04 16:55:41 -0400

    Initialized the menu in the MainActivity and changed the toolbar color

--- app/src/main/java/hiseholuwa/toolbar/MainActivity.java
+++ app/src/main/java/hiseholuwa/toolbar/MainActivity.java
@@ -3,6 +3,9 @@ package hiseholuwa.toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.item1_id){
+            Toast.makeText(this, "Item 1 is selected", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.item2_id){
+            Toast.makeText(this, "Item 2 is selected", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.item3_id){
+            Toast.makeText(this, "Item 3 is selected", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
